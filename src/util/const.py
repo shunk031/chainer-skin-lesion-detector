8ba7a038d4156ea6c55dfa7a97ce8f60fb11d4f7
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2018-11-18 01:29:21 +0900

    add some consts

--- src/util/const.py
+++ src/util/const.py
@@ -1,11 +1,27 @@
 import pathlib
 
+# /
 ROOT_DIR = pathlib.Path(__file__).parents[2]
-PINK_DIR = ROOT_DIR / 'pink'
+
+# /data
 DATA_DIR = ROOT_DIR / 'data'
+
+# /data/ISIC2018_Task1_Training_GroundTruth
 GT_DIR = DATA_DIR / 'ISIC2018_Task1_Training_GroundTruth'
+
+# /data/ISIC2018_Task1-2_Training_Input
 TRAIN_DIR = DATA_DIR / 'ISIC2018_Task1-2_Training_Input'
 
+# /data/preprocessed
+PREPROCESSED_DIR = DATA_DIR / 'preprocessed'
+
+# /data/preprocessed/ground_truth
+PREPROCESSED_GT_DIR = PREPROCESSED_DIR / 'ground_truth'
+
+# /data/preprocessed/input
+PREPROCESSED_TRAIN_DIR = PREPROCESSED_DIR / 'input'
+
+MAX_SIZE = 700
 LABELS = ['lesion']
 
 VALIDATION_SIZE = 0.2
